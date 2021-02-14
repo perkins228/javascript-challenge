cb06d9dc293bbc427cabe52bd31cb7384059a1e6
perkins228 <perkins.228@gmail.com>
2021-02-13 21:34:19 -0500

    function test

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,20 +1,31 @@
 // from data.js
 let tableData = data;
 
-let dateTime = tableData.map(i => i.datetime);
-let city = tableData.map(i => i.city);
-let state = tableData.map(i => i.state);
-let country = tableData.map(i => i.country);
-let shape = tableData.map(i => i.shape);
-let time = tableData.map(i => i.durationMinutes);
-let comments = tableData.map(i => i.comments);
-
 d3.select('#filter-btn').on('click', insertTable);
 d3.select('form').on('submit', insertTable)
+
 
 
 function insertTable() {
     d3.event.preventDefault();
     let submitDate = d3.select('#datetime').node().value;
-    console.log(submitDate)
+    d3.select('#datetime').node().value = null;
+    let table = d3.select('#ufo-table')
+    let tbody = table.select('tbody');
+    let trow
+    let filteredData = tableData.filter(i => i.datetime === submitDate)
+    let dateTime = filteredData.map(i => i.datetime);
+    let city = filteredData.map(i => i.city);
+    let state = filteredData.map(i => i.state);
+    let country = filteredData.map(i => i.country);
+    let shape = filteredData.map(i => i.shape);
+    let time = filteredData.map(i => i.durationMinutes);
+    let comments = filteredData.map(i => i.comments);
+    for (let i = 0; i < filteredData.length; i++){
+        trow = tbody.append("tr");
+        trow.append("td").text(dateTime[i]);
+        trow.append("td").text(city[i]);
+    }
+
+
 }
